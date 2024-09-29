e530f0d35648f4be8196b01b2d25963b38022244
Eduard <edik.moskalets@gmail.com>
2024-09-29 11:29:37 +0300

    step 2

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+    // Читаємо файл за шляхом PATH_DB
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    
+    // Парсимо JSON та повертаємо результат
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    throw new Error('Failed to read contacts');
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,17 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+    // Перетворюємо оновлені контакти в формат JSON
+    const data = JSON.stringify(updatedContacts, null, 2);
+    
+    // Записуємо дані у файл
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    
+    console.log('Contacts have been successfully updated.');
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw new Error('Failed to write contacts');
+  }
+};
