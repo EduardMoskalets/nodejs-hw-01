66b3f7cf9cc8aafec6b920c07fc0286c45ceb766
Eduard <edik.moskalets@gmail.com>
2024-09-29 14:49:15 +0300

    step 5

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,14 @@
-export const getAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+export const getAllContacts = async () => {
+  try {
+    let dbData = [];
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    return (dbData = JSON.parse(data));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await getAllContacts());
