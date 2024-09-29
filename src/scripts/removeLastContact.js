aa5bf593f30ef953a9500df26f8c8f8ce11c4d85
Eduard <edik.moskalets@gmail.com>
2024-09-29 16:50:13 +0300

    step 8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+export const removeLastContact = async () => {
+try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const dbData = JSON.parse(data);
+    
+    if (dbData.length > 0) {
+      dbData.pop();
+      console.log(dbData);
+      await fs.writeFile(PATH_DB, JSON.stringify(dbData, undefined, 2));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+};
 
 removeLastContact();
