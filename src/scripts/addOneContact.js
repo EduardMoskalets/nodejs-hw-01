f378464ea17b9eb69e8ccde31c7b8856fe8d24fe
Eduard <edik.moskalets@gmail.com>
2024-09-29 14:02:33 +0300

    step 4

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,21 @@
-export const addOneContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => { 
+ try {
+    let dbData = [];
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    dbData = JSON.parse(data);
+
+    const oneContact = createFakeContact();
+    dbData.push(oneContact);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(dbData, undefined, 2));
+  } catch (error) {
+    console.error(error);
+  }
+
+};
 
 addOneContact();
