fe44d4a32aa572cdcb624eddef984c3312c00d2c
Eduard <edik.moskalets@gmail.com>
2024-09-29 14:58:10 +0300

    step 6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,17 @@
-export const countContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+
+export const countContacts = async () => { 
+try {
+    let dbData = [];
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    dbData = JSON.parse(data);
+    return dbData.length;
+  } catch (error) {
+    console.error(error);
+  }
+
+};
 
 console.log(await countContacts());
